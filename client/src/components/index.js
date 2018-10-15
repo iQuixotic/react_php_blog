a2a55ca8f08b1605461132b68cfcefb22919bd48
Trey <treyw1850@gmail.com>
2018-10-15 06:43:23 -0400

    added backdrop and sidedrawer

---
+++ client/src/components/index.js
@@ -0,0 +1,28 @@
+// // export * from brand
+// export { default as Logo } from './brand/Logo';
+
+// // export * from structure
+// export { default as Header } from './structure/account/header/header';
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// export { default as Col } from './structure/grid/col';
+// export { default as Container } from './structure/grid/container';
+// export { default as Div } from './structure/grid/div';
+// export { default as Row } from './structure/grid/row';
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+// export * from nav
+export { default as Sidedrawer } from './nav/sidedrawer/sidedrawer';
+export { default as Burger } from './nav/sidedrawer/burger/burger';
+// export { default as NavItems } from './nav/navItems/navItems';
+// export { default as Toolbar } from './nav/toolbar/toolbar';
+
+// export * from UI
+// export { default as Button } from './UI/button/button';
+// export { default as Input } from './UI/input/input';
+
+// export * from visual
+export { default as Backdrop } from './visual/backdrop/backdrop';
+
